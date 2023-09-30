574afc6f5cea6801d869fa170c653e30b328fbb2
Takoutche Silvestre Armel <takoutchea@gmail.com>
2023-09-30 02:05:11 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'TP1TAKOUTCHE',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
